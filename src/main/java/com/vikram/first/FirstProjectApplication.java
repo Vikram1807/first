dfbf458ddd398823694801c67d9d4c8edbea3d40
vikram <dehariyavikram01@gmail.com>
2024-02-29 23:43:46 +0530

    added swagger url for convience

--- src/main/java/com/vikram/first/FirstProjectApplication.java
+++ src/main/java/com/vikram/first/FirstProjectApplication.java
@@ -9,6 +9,8 @@ public class FirstProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FirstProjectApplication.class, args);
+        System.out.println("use swagger here : http://localhost:8080/swagger-ui/index.html ");
+        System.out.println("or u can also use postman");
     }
 
 }
